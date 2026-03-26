d69d69b3432a3ffd0c709b3120a66576991d0aeb
Mikolaj982 <107889765+Mikolaj982@users.noreply.github.com>
2026-03-26 20:30:49 +0100

    feat: add delete group functionality with role handling and feedback messages

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import { useUserGroups } from '../../hooks/useUserGroups';
-import { Group } from './types/group.types';
+import { UserGroup } from './types/group.types';
 import CreateGroupForm from '../../components/CreateGroup/CreateGroupForm';
+import { deleteGroup } from '../../services/deleteGroup';
+import { toast } from 'react-toastify';
+import { MESSAGES } from '../../utils/messages';
 
 const Dashboard: React.FC = () => {
     const { groups, loading, error, refetchGroups } = useUserGroups();
+    const handleDeleteGroup = async (groupId: string) => {
+        if (!groupId) return;
+        if (!window.confirm('Jesteś pewien?')) return;
+        try {
+            await deleteGroup(groupId);
+            await refetchGroups();
+            toast.success(MESSAGES.SUCCES.DELETED_GROUP)
+        } catch (error) {
+            if (error instanceof Error) {
+                toast.error(error.message);
+            } else {
+                toast.error(MESSAGES.ERROR.UNKNOWN);
+            }
+        }
+    };
+
     return (
         <div className='bg-indigo-400 h-[100vh] w-[100vw]'>
             <h2>Hello</h2>
@@ -12,13 +31,16 @@ const Dashboard: React.FC = () => {
             {loading ? <p> Loading...</p> : (
                 <ul>
                     {
-                        groups.map((group: Group) => (
-                            <li key={group.id}>{group.name}</li>
+                        groups.map((group: UserGroup) => (
+                            <li key={group.id}>
+                                <h3>{group.name}</h3>
+                                {group.role === 'owner' && <button onClick={() => handleDeleteGroup(group.id)}>delete</button>}
+                            </li>
                         ))
                     }
                 </ul>
             )}
-            {error ?? <p>{error}</p>}
+            {error && <p>{error}</p>}
             <CreateGroupForm onSuccess={refetchGroups} />
         </div>
     );
