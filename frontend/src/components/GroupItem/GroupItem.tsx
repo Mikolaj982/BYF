731c2233ae4d6f2147cad768918d97119d4db14b
Mikolaj982 <107889765+Mikolaj982@users.noreply.github.com>
2026-03-29 13:18:24 +0200

    feat: add lobby creation with fetching and fix group deletion constraints

---
+++ frontend/src/components/GroupItem/GroupItem.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { UserGroup } from '../../pages/Dashboard/types/group.types';
+import UpdateGroupForm from '../UpdateGroup/UpdateGroupForm';
+import CreateLobbyForm from '../CreateLobby/CreateLobbyForm';
+import Lobbies from '../Lobbies/Lobbies';
+import { deleteGroup } from '../../services/deleteGroup';
+import { toast } from 'react-toastify';
+import { MESSAGES } from '../../utils/messages';
+import { useGroupLobbies } from '../../hooks/useGroupLobbies';
+
+const GroupItem: React.FC<{ groupData: UserGroup, refetchGroups: () => Promise<void> }> = ({ groupData, refetchGroups }) => {
+
+  const { id, name, description, role } = groupData;
+  const { lobbies, refetchLobbies } = useGroupLobbies(id);
+
+  const handleDeleteGroup = async (groupId: string) => {
+    if (!window.confirm('Jesteś pewien?')) return;
+    try {
+      await deleteGroup(groupId);
+      await refetchGroups();
+      toast.success(MESSAGES.SUCCESS.DELETED_GROUP)
+    } catch (error) {
+      if (error instanceof Error) {
+        toast.error(error.message);
+      } else {
+        toast.error(MESSAGES.ERROR.UNKNOWN);
+      }
+    }
+  };
+
+  return (
+    <li>
+      <h3>{name}</h3>
+      <p>{description}</p>
+      {role === 'owner' && (
+        <div>
+          <button onClick={() => handleDeleteGroup(id)}>delete</button>
+          <UpdateGroupForm onSuccess={refetchGroups} groupData={groupData} />
+          <CreateLobbyForm onSuccess={refetchLobbies} groupData={groupData} />
+        </div>
+      )}
+      <Lobbies lobbies={lobbies} />
+    </li>
+  )
+}
+
+export default GroupItem;
