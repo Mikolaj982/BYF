a72ddcd8c1c3a000304c6ddb0e491f161af88ef7
Mikolaj982 <107889765+Mikolaj982@users.noreply.github.com>
2026-04-01 19:15:30 +0200

    refactor: replace magic string error with constant

--- frontend/src/services/lobbies/joinLobby.ts
+++ frontend/src/services/lobbies/joinLobby.ts
@@ -1,5 +1,6 @@
 import { supabase } from "../../shared/api/supabaseClient";
 import { LobbyMember } from "../../pages/Dashboard/types/lobby.types";
+import { DB_ERROR_CODES } from "../../constants/dberrors";
 
 export async function joinLobby(lobbyData: LobbyMember) {
     const { user_id: userId, lobby_id: lobbyId } = lobbyData;
@@ -10,7 +11,7 @@ export async function joinLobby(lobbyData: LobbyMember) {
             lobby_id: lobbyId
         }])
     if (error) {
-        if (error.code === "23505") {
+        if (error.code === DB_ERROR_CODES.UNIQUE_VIOLATION) {
             throw new Error("Już jesteś w lobby");
         }
         throw error;
