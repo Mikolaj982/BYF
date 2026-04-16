ecd56a92b46b2242062056038ce87a52fa915545
Mikolaj982 <107889765+Mikolaj982@users.noreply.github.com>
2026-04-16 19:21:20 +0200

    feat: implement matches fetching with data mapping

---
+++ frontend/src/services/lobbies/lobbyMatchesService.ts
@@ -0,0 +1,49 @@
+import { supabase } from "../../shared/api/supabaseClient";
+import { Match } from "../../pages/Dashboard/types/lobby.types";
+
+export async function getLobbyMatches(lobbyId: string) {
+    const { data, error } = await supabase
+        .from('matches')
+        .select(`
+            id,
+            match_participants (
+                user_id,
+                profiles (username)
+            ),
+            match_scores (
+                user_id,
+                score
+            )
+            `)
+        .eq('lobby_id', lobbyId);
+
+    if (error) throw error;
+
+    const mapped: Match[] = data?.map((match) => {
+        const scores = match.match_scores;
+        const participants = match.match_participants;
+
+        const players = participants.map(p => {
+            if (!p.user_id) throw new Error('Missing user_id')
+
+            const profile = Array.isArray(p.profiles)
+                ? p.profiles[0]
+                : p.profiles;
+
+            const score = scores.find(s => s.user_id === p.user_id);
+
+            return {
+                userId: p.user_id,
+                username: profile?.username ?? 'unknown',
+                score: score?.score ?? 0
+            }
+        });
+
+        return {
+            matchId: match.id,
+            players,
+        };
+    }) || [];
+
+    return mapped;
+};
