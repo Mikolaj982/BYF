1589f5087f10d5203451d8d01dba4e75093097d9
Mikolaj982 <107889765+Mikolaj982@users.noreply.github.com>
2026-04-27 19:25:24 +0200

    refactor: replace createGroup and deleteGroup services with RPC

--- frontend/src/services/createGroup.ts
+++ frontend/src/services/createGroup.ts
@@ -1,25 +1,13 @@
 import { supabase } from "../shared/api/supabaseClient";
 import { CreateGroupSubmitData } from "../components/CreateGroup/CreateGroupForm";
 
-export async function createGroup(createGroupData: CreateGroupSubmitData) {
-    const invite_code = Math.random().toString(36).substring(2, 8).toUpperCase()
+export async function createGroup(createGroupData: CreateGroupSubmitData): Promise<string> {
     const { name, description, owner } = createGroupData;
-    const { data: group, error: groupError } = await supabase
-        .from('groups')
-        .insert([{ name, description, owner, invite_code }])
-        .select()
-        .single();
-
-    if (groupError) throw groupError;
-
-    const { error: membersError } = await supabase
-        .from('group_members')
-        .insert([{
-            group_id: group.id,
-            user_id: owner,
-            role: 'owner',
-        }])
-
-    if (membersError) throw membersError;
-    return group;
+    const { data: id, error } = await supabase.rpc('create_group', {
+        input_name: name,
+        input_description: description,
+        input_owner: owner,
+    });
+    if (error) throw error;
+    return id;
 };
