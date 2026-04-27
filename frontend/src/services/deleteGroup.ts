931f53bf4e2f7bd1b3a6c7a8ca283affedd7dc0a
Mikolaj982 <107889765+Mikolaj982@users.noreply.github.com>
2026-04-27 19:25:24 +0200

    refactor: replace createGroup and deleteGroup services with RPC

--- frontend/src/services/deleteGroup.ts
+++ frontend/src/services/deleteGroup.ts
@@ -1,40 +1,10 @@
 import { supabase } from "../shared/api/supabaseClient";
 
 export async function deleteGroup(groupId: string) {
-    const { data: lobbies } = await supabase
-        .from("lobbies")
-        .select("id")
-        .eq("group_id", groupId);
+    if (!groupId) throw new Error('Wymagany identyfikator grupy.')
 
-    const lobbyId = lobbies?.map(lobby => lobby.id) ?? [];
+    const { error } = await supabase.rpc('delete_group', { input_group_id: groupId });
 
-    if (lobbyId.length > 0) {
-        const { error: deleteLobbiesMembersError } = await supabase
-            .from('lobby_members')
-            .delete()
-            .in('lobby_id', lobbyId);
-
-        if (deleteLobbiesMembersError) throw deleteLobbiesMembersError;
-    };
-
-    const { error: deleteLobbiesError } = await supabase
-        .from('lobbies')
-        .delete()
-        .eq('group_id', groupId);
-
-    if (deleteLobbiesError) throw deleteLobbiesError;
-
-    const { error: deleteGroupMembersError } = await supabase
-        .from('group_members')
-        .delete()
-        .eq('group_id', groupId);
-
-    if (deleteGroupMembersError) throw deleteGroupMembersError;
-
-    const { error: deleteGroupError } = await supabase
-        .from('groups')
-        .delete()
-        .eq('id', groupId);
-
-    if (deleteGroupError) throw deleteGroupError;
+    if (error) throw error;
+    return;
 };
