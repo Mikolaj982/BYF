066f351f7f3809ba3f677f199b2bbb0652a77ec0
Mikolaj982 <107889765+Mikolaj982@users.noreply.github.com>
2026-05-12 12:41:21 +0200

    refactor: migrate invite box to MUI

---
+++ frontend/src/features/groups/components/GroupMainContent/GroupMainContent.tsx
@@ -0,0 +1,12 @@
+import { Stack } from '@mui/material'
+import React from 'react'
+
+const GroupMainContent = () => {
+    return (
+        <Stack>
+
+        </Stack>
+    )
+}
+
+export default GroupMainContent
