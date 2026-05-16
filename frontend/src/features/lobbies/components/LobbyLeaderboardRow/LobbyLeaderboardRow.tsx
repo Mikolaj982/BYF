27048022d169b06c51e5a35dbb49d55e063a12a4
Mikolaj982 <107889765+Mikolaj982@users.noreply.github.com>
2026-05-16 16:39:05 +0200

    refactor: implement MUI leaderboard UI with rank progress bars

--- frontend/src/features/lobbies/components/LobbyLeaderboardRow/LobbyLeaderboardRow.tsx
+++ frontend/src/features/lobbies/components/LobbyLeaderboardRow/LobbyLeaderboardRow.tsx
@@ -1,16 +1,63 @@
+import { Avatar, LinearProgress, Stack, Typography } from '@mui/material';
 import React from 'react';
+import { Leaderboard } from '../../services/lobbyLeaderboard';
 
 type LobbyLeaderboardRowProps = {
-    username: string;
-    score: number;
+    playerData: Leaderboard;
+    place: number;
+    maxScore: number;
 };
 
-const LobbyLeaderboardRow: React.FC<LobbyLeaderboardRowProps> = ({ username, score }) => {
+const LobbyLeaderboardRow: React.FC<LobbyLeaderboardRowProps> = (
+    {
+        playerData,
+        place,
+        maxScore
+    }
+) => {
+    const progressValue = maxScore > 0
+        ? (playerData.total_score / maxScore) * 100
+        : 0;
+
     return (
-        <li>
-            {username} : {score}
-        </li>
-    );
-};
+        <Stack
+            direction='row'
+            alignItems='center'
+            sx={{
+                gap: 3,
+                px: 3,
+                py: 2,
+                borderRadius: 0,
+                borderColor: '#2A2A2A',
+                transition: '0.2s ease',
+                '&:hover': {
+                    backgroundColor: '#1e1e1e',
+                },
+            }}
+        >
+            <Typography
+                color='#2a2a2a'
+                fontWeight='bold'
+            >
+                {place}
+            </Typography>
+            <Avatar>
+            </Avatar>
+            <Typography>
+                {playerData.username}
+            </Typography>
+            <Stack marginLeft='auto' direction='row' alignItems='center' spacing={4}>
+                <LinearProgress
+                    variant='determinate'
+                    value={progressValue}
+                    sx={{ width: 100, height: 6, borderRadius: 1 }}
+                />
+                <Typography>
+                    {playerData.total_score}
+                </Typography>
+            </Stack>
+        </Stack>
+    )
+}
 
-export default LobbyLeaderboardRow;
+export default LobbyLeaderboardRow
